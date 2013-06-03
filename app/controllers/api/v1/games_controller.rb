b00025d67b41eb1eec1976f846727fcf6b37a8ac
Josh McArthur <joshua.mcarthur@gmail.com>
2013-06-03 18:31:00 +1200

    Un-nest games controller

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,15 +1,11 @@
 class Api::V1::GamesController < API::V1::BaseController
   def index
-    @games = Rails.cache.fetch "api-v1-#{player.id}-games", :expires_in => 1.minute do
-      player.games.complete.order(:updated_at)
+    @games = Rails.cache.fetch "api-v1-games", :expires_in => 1.minute do
+      Game.complete.order(:updated_at)
     end
 
     respond_to do |format|
       format.json { render }
     end
-  end
-
-  def player
-    @player ||= Player.find(params[:player_id])
   end
 end
